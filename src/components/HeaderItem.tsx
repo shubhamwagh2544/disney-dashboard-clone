bf1e6709ded898f91622843f9568d3a98148267f
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-08 10:49:43 +0530

    production list added

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -1,6 +1,6 @@
 export const HeaderItem = ({ name, Icon }: any) => {
     return (
-        <div className="text-white text-[20px] font-medium cursor-pointer flex items-center gap-3 hover:underline underline-offset-8 mb-2">
+        <div className="text-white text-[20px] font-medium cursor-pointer flex items-center gap-3 hover:underline underline-offset-8 mb-2 mt-2">
             <Icon />
             <h2 className="">{name}</h2>
         </div>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -27,14 +27,14 @@ export const Slider = () => {
 
     return (
         <div>
-            <HiChevronLeft className="hidden md:block text-white text-[30px] absolute mx-8 mt-[250px] cursor-pointer" onClick={() => slideLeft(slideReference.current)} />
-            <HiChevronRight className="hidden md:block text-white text-[30px] absolute mx-8 mt-[250px] cursor-pointer right-0" onClick={() => slideRight(slideReference.current)} />
+            <HiChevronLeft className="hidden md:block text-white text-[35px] absolute mx-8 mt-[175px] cursor-pointer left-0" onClick={() => slideLeft(slideReference.current)} />
+            <HiChevronRight className="hidden md:block text-white text-[35px] absolute mx-8 mt-[175px] cursor-pointer right-0" onClick={() => slideRight(slideReference.current)} />
 
             <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth" ref={slideReference}>
                 {
-                    movies.map((item): any => (
-                        <img src={IMAGE_BASE_URL + item.backdrop_path}
-                            className="min-w-full object-cover h-[500px] object-top ml-3 mr-3 rounded-lg hover:border-[2px] border-gray-400 transition-all duration-100 ease-in-out" />
+                    movies.map((item, index): any => (
+                        <img src={IMAGE_BASE_URL + item.backdrop_path} key={index}
+                            className="min-w-full  md:object-cover h-[350px] object-top ml-2 mr-5 rounded-lg hover:border-[2px] border-gray-400 transition-all duration-100 ease-in-out" />
                     ))
                 }
             </div>
